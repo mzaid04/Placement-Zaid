7ec99d89ad4cf680aecaee3950eff4db838dccd1
mzaid04 <150179006+mzaid04@users.noreply.github.com>
2023-11-08 13:29:16 +0500

    Placement by zaid! done

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './Pages/**/*.{cshtml,razor}',
+        './Shared/**/*.{cshtml,razor}'
+    ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
